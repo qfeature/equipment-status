85fa4061bd469988d7343aefa93e42c49c60671a
qfeature <qfeature@users.noreply.github.com>
2021-10-19 00:04:24 -0700

    Add DeleteEquipment lambda. Enable iam roles per function

--- backend/src/helpers/equipmentAcess.ts
+++ backend/src/helpers/equipmentAcess.ts
@@ -1,11 +1,11 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+//import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { EquipmentItem } from '../models/EquipmentItem'
 import { EquipmentUpdate } from '../models/EquipmentUpdate'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+//const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('equipmentAccess')
 
@@ -120,11 +120,11 @@ export class EquipmentAccess {
 function createDynamoDBClient() {
     if (process.env.IS_OFFLINE) {
         logger.info('Creating a local DynamoDB instance')
-        return new XAWS.DynamoDB.DocumentClient({
+        return new AWS.DynamoDB.DocumentClient({
             region: 'localhost',
             endpoint: 'http://localhost:8000'
         })
     }
 
-    return new XAWS.DynamoDB.DocumentClient()
+    return new AWS.DynamoDB.DocumentClient()
 }
