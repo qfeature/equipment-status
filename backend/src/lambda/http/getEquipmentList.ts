6505936938635ce72c1f9f9fb97f10be33b7bb14
qfeature <qfeature@users.noreply.github.com>
2021-10-30 01:25:26 -0700

    Add latency metric for GetEquipmentList lambda. Add IAM policy to write to CloudWatch

--- backend/src/lambda/http/getEquipmentList.ts
+++ backend/src/lambda/http/getEquipmentList.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-import { getEquipmentListForUser /*, timeInMs, setLatencyMetric*/ } from '../../helpers/equipment'
+import { getEquipmentListForUser, timeInMs, setLatencyMetric } from '../../helpers/equipment'
 import { getUserId } from '../utils';
 import { createLogger } from '../../utils/logger'
 const logger = createLogger('getEquipmentList')
@@ -11,18 +11,28 @@ const logger = createLogger('getEquipmentList')
 
 export const handler = middy(
    async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-      logger.info('Processing GetEquipmentList event', event)
+      const startTime = timeInMs() // Record time start
+      try {
+         logger.info('Processing GetEquipmentList event', event)
 
-      const userId = getUserId(event)
-      const eqList = await getEquipmentListForUser(userId)
+         const userId = getUserId(event)
+         const eqList = await getEquipmentListForUser(userId)
 
-      logger.info('Equipment list found', eqList)
+         logger.info('Equipment list found', eqList)
 
-      return {
-         statusCode: 200,
-         body: JSON.stringify({
-            items: eqList
-         })
+         return {
+            statusCode: 200,
+            body: JSON.stringify({
+               items: eqList
+            })
+         }
+      } catch (e) {
+         logger.error('GetEquipmentList error', { error: JSON.stringify(e) })
+         throw new Error(e)
+      } finally {
+         const endTime = timeInMs(); // Record time finished
+         const totalTime = endTime - startTime;
+         await setLatencyMetric('GetEquipmentListMetric', totalTime)
       }
    }
 )
