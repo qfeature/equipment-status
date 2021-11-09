d9784ceca6892db2d0acf5a0f77a646e4de5becf
qfeature <qfeature@users.noreply.github.com>
2021-11-08 23:41:45 -0800

    Package deployment as individual zip. Add config file. Add screen shots of backend deployment

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // API endpoint information
-const apiId = ''
+const apiId = 'dx1pw0juk4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
 
 export const authConfig = {
   // Create an Auth0 application and copy values from it into this map.
-  domain: '',          // Auth0 domain
-  clientId: '', // Auth0 client id
-  callbackUrl: ''
+  domain: 'dev-i9zmlvbg.us.auth0.com',          // Auth0 domain
+  clientId: '8ypow6CEssN5KmLdUORaqX8CFZghHH0M', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
 }
